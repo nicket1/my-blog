918f8d8fe6508bb707e96f2dac6566c85b097c01
nicket <2573155492@qq.com>
2021-10-31 21:29:51 +0800

    仍有bug主页

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask,render_template
+import datetime
 app = Flask(__name__)
+
+todo=[
+    {'date':'10.31','busness':'划水'},
+    {'date':'11.01','busness':'白嫖'},
+    {'date':'11.02','busness':'划水'},
+    {'date':'11.03','busness':'划水'},
+    {'date':'11.04','busness':'划水'}
+]
+year=datetime.datetime.now().year
+mouth=datetime.datetime.now().month
+day=datetime.datetime.now().day
 
 @app.route('/')
 def hello():
-    return render_template('index.html');
+    return render_template('index.html',todo=todo,year=year,mouth=mouth,day=day);
